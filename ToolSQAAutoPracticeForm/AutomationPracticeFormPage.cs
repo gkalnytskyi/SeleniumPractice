ddd2bc7bd484428e216c5050640a000a80807b00
Georgii Kalnytskyi <gkalnytskyi@gmail.com>
2015-10-18 17:03:50 +1000

    Extract FormWidget from AutomationPracticeFormPage class

--- ToolSQAAutoPracticeForm/AutomationPracticeFormPage.cs
+++ ToolSQAAutoPracticeForm/AutomationPracticeFormPage.cs
@@ -14,20 +14,6 @@ namespace ToolSQAAutoPracticeFormFramework
             _AF = af;
         }
 
-        public class FormWidget
-        {
-            protected AutomationFramework _AF;
-            protected IWebElement BaseElement { get; private set; }
-
-            public FormWidget (AutomationFramework af)
-            {
-                if (af == null)
-                {
-                    throw new ArgumentException("Automation framework is not provided");
-                }
-                _AF = af;
-                BaseElement = _AF.Driver.FindElement(By.ClassName("form-horizontal"));
-            }
-        }
+        
     }
 }
---
+++ ToolSQAAutoPracticeForm/FormWidget.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+
+namespace ToolSQAAutoPracticeFormFramework
+{
+    public class FormWidget
+    {
+        protected AutomationFramework _AF;
+        protected IWebElement BaseElement { get; private set; }
+
+        public FormWidget(AutomationFramework af)
+        {
+            if (af == null)
+            {
+                throw new ArgumentException("Automation framework is not provided");
+            }
+            _AF = af;
+            BaseElement = _AF.Driver.FindElement(By.ClassName("form-horizontal"));
+        }
+
+        public IWebElement FistNameTextBox
+        {
+            get
+            {
+                return BaseElement.FindElement(By.Name("firstname"));
+            }
+        }
+
+        public IWebElement LastNameTextBox
+        {
+            get
+            {
+                return BaseElement.FindElement(By.Name("lastname"));
+            }
+        }
+    }
+}
